27ba40fa47bf8ffa740028900138da0c3531f684
Robert S Pratt <59897760+RobertSPratt@users.noreply.github.com>
2020-07-09 15:15:56 -0400

    Update a09.py

--- a09.py
+++ a09.py
@@ -1,3 +1,8 @@
+#IT 2819 - Python Programming
+#Prof. Weisfeld
+#Robert Pratt
+#Assignment 09 - A09-Pratt.py
+
 import math
 
 class Shape:
@@ -17,18 +22,28 @@ class Shape:
             print("Side",i+1,"is",self.sides[i])
 
 class Triangle(Shape):
-    #inherits __init__ function
+    def __init__(self, no_of_sides):
+        if no_of_sides > 3:
+            print ("Error: Triangles can only have 3 sides")
+        else:
+            Shape.__init__(self, no_of_sides)
 
-# Cannot find a way to get the values out of the generator self.sides to use them in my
-# equations below. Any ideas on how I can do that without modifying the give code above?
     def findArea(self):
-        print ("Computing Triangle's Area")
-        # Find semi_perimeter, which is all sides added, divided by 2.
-        semi_perimeter = ((semi_perimeter + next(self.sides)) for i in range(self.n))
-        # triangle_area = sqrt(semi_perimeter * (s_p - s1)(s_p - s2)(s_p - s3))
+        print ("\nInside Triangle Constructor \n")
+
+        self.inputSides()
+        self.dispSides()
+
+        semi_perimeter = 0
+        for i in self.sides:
+            semi_perimeter += i
+        semi_perimeter /= 2
+        side = [float(semi_perimeter - i) for i in self.sides]
+        triangle_area = math.sqrt(semi_perimeter * side[0] * side[1] * side[2])
+        print ("\nComputing Triangle's Area \n")
+        print ("The area of this triangle is: " + format(triangle_area, '.2f'))
+        
 
 print("Program by Robert Pratt\n")
 t = Triangle(3)
-t.inputSides()
-t.dispSides()
 t.findArea()
