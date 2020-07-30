1ae84f5d048535c3b8867b47fede902da645ff57
Robert S Pratt <59897760+RobertSPratt@users.noreply.github.com>
2020-07-29 20:05:35 -0400

    Create Chapter17Problem3.java

---
+++ Chapter17Problem3.java
@@ -0,0 +1,24 @@
+/*
+ * IT-2650 Java Programming, Summer 2020
+ * Instructor: Martin P. Walsh
+ * Student Name: Robert PRatt
+ * Homework Assignment: Ch. 17, Problem 3
+ * Purpose of Assignment:
+ */
+
+ package chapter17problem3;
+ 
+public class Chapter17Problem3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("IT-2650 Java Programming");
+        System.out.println("Student Name: Robert Pratt");
+        System.out.println("Homework Assignment: Chapter 17, Problem 3");
+        System.out.println("_____________________________");
+        System.out.println("");
+
+        Calculator aCalculator = new Calculator();
+        aCalculator.setVisible(true);
+    }
+}
